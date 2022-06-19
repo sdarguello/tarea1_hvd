9ac992f076ff5ee31f05a8140d893a14da39c206
r00tsh4rk <j.alejandrochimal92@gmail.com>
2022-06-19 18:19:31 -0500

    Mapa de México con JSON y D3

--- js/app.js
+++ js/app.js
@@ -1,33 +1,79 @@
-//Definiendo selectores
-const guadalupe = d3.select("#guadalupe")
-const santiago = d3.select("#santiago")
-const alex = d3.select("#alex")
+// Selecciones
+const graf = d3.select("#graf");
 
-//Estableciendo dimensiones de cada área de trabajo por elemento del equipo
-const anchoGua = +guadalupe.style("width").slice(0, -2)
-const altoGua = (anchoGua * 9) / 16
+// Dimensiones
+const anchoTotal = +graf.style("width").slice(0, -2);
+const altoTotal = (anchoTotal * 9) / 16;
 
-const anchoSan = +santiago.style("width").slice(0, -2)
-const altoSan = (anchoSan * 9) / 16
+const margins = {
+  top: 60,
+  right: 20,
+  bottom: 75,
+  left: 100,
+};
+const ancho = anchoTotal - margins.left - margins.right;
+const alto = altoTotal - margins.top - margins.bottom;
 
-const anchoAle = +alex.style("width").slice(0, -2)
-const altoAle = (anchoAle * 9) / 16
-
-//Aérturando elementos SVG como área de trabajo de las gráficas.
-const svg_guadalupe = guadalupe
+// Elementos gráficos (layers)
+const svg = graf
   .append("svg")
-  .attr("width", anchoGua)
-  .attr("height", altoGua)
-  .attr("class", "guadalupe")
+  .attr("width", anchoTotal)
+  .attr("height", altoTotal)
+  .attr("class", "mapa");
 
-  const svg_santiago = santiago
-  .append("svg")
-  .attr("width", anchoSan)
-  .attr("height", altoSan)
-  .attr("class", "santiago")
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${margins.left}, ${margins.top})`);
 
-  const svg_alex = alex
-  .append("svg")
-  .attr("width", anchoAle)
-  .attr("height", altoAle)
-  .attr("class", "alex")
+//DEFINIENDO ELEMENTO GRÁFICOS DE LA LIBERÍA D3.GEO QUE MOSTRARÁN EL MAPA
+var projection = d3
+  .geoMercator()
+  .scale(600)
+  .translate([anchoTotal, altoTotal / 2]);
+var geoPath = d3.geoPath().projection(projection);
+
+const load = async (variable = "casos_totales") => {
+  // Carga de Datos
+  data = await d3.csv("dengue.csv", d3.autoType);
+  geoMexico = await d3.json("mexico.json", d3.autoType);
+
+  // DEFINIENDO ACCESORS
+  const yAccessor = (d) => d[variable];
+  const xAccessor = (d) => d.abreviatura;
+
+  const caracteristicas = geoMexico.features;
+  //geoArea nos da el area dado un GeoJson
+  //extent nos devuelve el minimo y maximo valor
+
+  var tamanioCarMex = d3.extent(caracteristicas, function (d) {
+    return d3.geoArea(d);
+  });
+
+  var newFeatureColor = d3.scaleQuantize().domain(tamanioCarMex);
+
+  data.sort((a, b) => yAccessor(b) - yAccessor(a));
+
+  console.log(data);
+  console.log(d3.map(data, xAccessor));
+
+  const map = g
+    .selectAll("path")
+    .data(geoMexico.features)
+    .enter()
+    .append("path")
+    .attr("d", geoPath)
+    .attr("id", (d) => d.id)
+    .attr("class", "estados")
+    .style("fill", (d) => {
+      //AQUI SE USA LA ESCALA DE COLORES EN BASE AL AREA DE CADA PAIS
+
+      if (d.id != "MX-YUC") {
+        return newFeatureColor(d3.geoArea(d));
+      } else {
+        return "red";
+      }
+    })
+    .style("stroke", (d) => d3.rgb(newFeatureColor(d3.geoArea(d))).darker());
+};
+
+load("margen");
